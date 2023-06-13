d5c210922c2cecb946d5d7bf8041bd9ec137c53e
ersomenki <ersomenki@gmail.com>
2023-06-13 20:11:15 +0600

    new version

--- script.js
+++ script.js
@@ -1,65 +1,117 @@
-let input = document.querySelector('.input'),
-      list = document.querySelector('.list'),
-      alert = document.querySelector('.alert'),
-      item = document.querySelector('.text');
-      
+window.addEventListener('DOMContentLoaded', ()=> {
+    const form = document.querySelector('.input-row'),
+          input = document.querySelector('.input'),
+          taskList = document.querySelector('.tasks'),
+          alert = document.querySelector('.alert');
 
-function showAlert() {
-    alert.classList.remove("hide");
-    alert.classList.add("show");
-}
 
-function closeAlert() {
-    alert.classList.remove("show");
-    alert.classList.add("hide")
-}
-
-function addTask() {
-    if (input.value === '') {
-        showAlert();
+    function showAlert() {
+        alert.classList.remove("hide");
+        alert.classList.add("show");
+    }
         
-    } else {
-        let li = document.createElement("li");
-        li.innerHTML = input.value;
-        li.classList.add("text");
-        list.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        span.classList.add("remove")
-        li.appendChild(span)
-        let edit = document.createElement("span");
-        edit.innerHTML = "&#9998";
-        edit.classList.add("pan");
-        li.appendChild(edit);
+    function closeAlert() {
+        alert.classList.remove("show");
+        alert.classList.add("hide")
     }
 
-    alert.addEventListener('click', (e)=>{
-        if (e.target = alert) {
-            closeAlert();
-        }
-    })
-    input.value = "";
-    saveData();
+    form.addEventListener('submit', (e)=> {
+        e.preventDefault();
+        if(input.value === '') {
+            showAlert();
+        } else {
+            const task = input.value;
+
+            const taskEl = document.createElement('div');
+            taskEl.classList.add('task');
+
+            const taskContentEl = document.createElement('div');
+            taskContentEl.classList.add('content');
+
+            taskEl.appendChild(taskContentEl);
+
+            const taskInputEl = document.createElement('input');
+            taskInputEl.classList.add('text');
+            taskInputEl.type = 'text';
+            taskInputEl.value = task;
+            taskInputEl.setAttribute('readonly', 'readonly');
+
+            taskContentEl.appendChild(taskInputEl);
+
+            const taskActionsEl = document.createElement('div');
+            taskActionsEl.classList.add('actions');
+
+            const taskFavoriteEl = document.createElement('button');
+            taskFavoriteEl.classList.add('favorite');
+            taskFavoriteEl.innerText = '\u2606';
+
+            const taskEditEl = document.createElement('button');
+            taskEditEl.classList.add('edit');
+            taskEditEl.innerText = '\u270E';
+
+            const taskRemoveEl = document.createElement('button');
+            taskRemoveEl.classList.add('remove');
+            taskRemoveEl.innerText = '\u00d7';
+
+            taskActionsEl.appendChild(taskEditEl);
+            taskActionsEl.appendChild(taskRemoveEl);
+            taskActionsEl.appendChild(taskFavoriteEl);
+
+            taskEl.appendChild(taskActionsEl);
+            taskList.appendChild(taskEl);
+
+            input.value = '';
+
+            taskEditEl.addEventListener('click', (e)=> {
+                if(taskEditEl.innerText.toLocaleLowerCase() == "\u270E") {
+                    taskEditEl.innerText = "\u2713";
+                    taskInputEl.removeAttribute("readonly");
+                    taskInputEl.focus();
+                } else {
+                    taskEditEl.innerText = "\u270E";
+                    taskInputEl.setAttribute("readonly", "readonly");
+                }
+            });
+
+            taskRemoveEl.addEventListener('click', ()=> {
+                taskList.removeChild(taskEl);
     
-}
+            })
 
-list.addEventListener('click', (e)=>{
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData();
-    } else if(e.target.classList == "remove") {
-        e.target.parentElement.remove();
-        saveData();
-    } else if(e.target.classList == "pan") {
-        item.contentEditable = true;
-    }
-}, false);
+            taskFavoriteEl.addEventListener('click', ()=>{
+                if(taskFavoriteEl.innerText.toLocaleLowerCase() == "\u2606") {
+                    taskFavoriteEl.classList.add('active');
+                    taskFavoriteEl.innerText = "\u2605";
+                } else {
+                    taskFavoriteEl.classList.remove('active');
+                    taskFavoriteEl.innerText = "\u2606";
+                }
+                
+            })
 
-function saveData(){
-    localStorage.setItem("data", list.innerHTML);
-};
+            let item = document.querySelector('text');
 
-function showTask(){
-    list.innerHTML = localStorage.getItem("data");
-}
-showTask();
+            item.addEventListener('click', ()=> {
+                item.classList.add('checked');
+            })
+
+            saveData()
+        }
+
+        document.addEventListener('click', (e)=>{
+            if (e.target != alert) {
+                closeAlert();
+            }
+        })
+
+        function saveData(){
+            localStorage.setItem("data", taskList.innerHTML);
+        };
+        
+        function showTask(){
+            taskList.innerHTML = localStorage.getItem("data");
+        }
+        showTask();
+        
+    });
+})
